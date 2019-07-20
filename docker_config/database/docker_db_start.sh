0cee6342a1732fb54ddda21c27b035357605a3eb
Maxx <45246893+mxfca@users.noreply.github.com>
2019-07-20 16:29:45 -0300

    added database container

---
+++ docker_config/database/docker_db_start.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# ########################################################################### #
+#
+# 				'########:'##:::::::'####::'######::'##:::'##::'######::
+# 				 ##.....:: ##:::::::. ##::'##... ##:. ##:'##::'##... ##:
+# 				 ##::::::: ##:::::::: ##:: ##:::..:::. ####::: ##:::..::
+# 				 ######::: ##:::::::: ##::. ######::::. ##::::. ######::
+# 				 ##...:::: ##:::::::: ##:::..... ##:::: ##:::::..... ##:
+# 				 ##::::::: ##:::::::: ##::'##::: ##:::: ##::::'##::: ##:
+# 				 ##::::::: ########:'####:. ######::::: ##::::. ######::
+# 				..::::::::........::....:::......::::::..::::::......:::
+#
+# ########################################################################### #
+# This file is part of FliSys.
+# 
+# FliSys is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version
+# 
+# FliSys is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details
+# 
+# You should have received a copy of the GNU General Public License
+# along with FliSys. If not, see <https://www.gnu.org/licenses/>.
+# ########################################################################### #
+# @author Maxx Fonseca
+# @copyright GNU General Public License 3
+# ########################################################################### #
+set -e
+trap "echo got traped signal, exiting..." HUP INT QUIT TERM
+
+# Start Apache Service
+# ----------------------------------------------------------------------------
+/usr/sbin/service mysql start
+
+# wait until receive stop signal
+# ----------------------------------------------------------------------------
+echo "[hit enter key to exit] or run 'docker stop <container>'"
+read
+
+# Stop Apache Service
+# ----------------------------------------------------------------------------
+/usr/sbin/service mysql stop
