2a386a967f179a91d3daca52b2556e826c3d8a30
Maxx <45246893+mxfca@users.noreply.github.com>
2019-08-10 17:36:20 -0300

    added new codes

--- docker_config/scripts/create_http_image.sh
+++ docker_config/scripts/create_http_image.sh
@@ -28,7 +28,7 @@
 # ########################################################################### #
 
 # Set Options
-# ###########################################################################
+# ################
 set -o errexit # abort on nonzero exit status
 set -o nounset # abort on unbound variable
 if test "${BASH_VERSION%%.*}" -gt '2'; then
@@ -40,18 +40,24 @@ fi
 declare SCRIPT_PATH
 declare HTTP_PATH
 declare DOCKER_FILE
+declare CONTAINER_ENVIRONMENT
 
 # Default Values
-# ###########################################################################
+# ################
 SCRIPT_PATH="$(cd "$(dirname "${0}")" && pwd -P)"
 HTTP_PATH="$(dirname "${SCRIPT_PATH}")/http"
-DOCKER_FILE="${HTTP_PATH}/Dockerfile"
+CONTAINER_ENVIRONMENT="production"
 
 # Add auxiliary script
-# ###########################################################################
+# ################
+# shellcheck source=/dev/null
 . "${SCRIPT_PATH}/util.sh"
 
-# Startup function, not logic in it, just flow
+# ########################################################################## #
+# Execution
+# ########################################################################## #
+
+# Startup function
 # ###########################################################################
 function main() {
   local valid_path
@@ -61,33 +67,35 @@ function main() {
   user_choice=''
 
   check_bash_version
+  get_arguments "${@}"
+  set_environment
 
   if test -z "$(path_exists "${HTTP_PATH}")"; then
-    echo "Path to FliSys HTTP Service not found. Exiting..."
+    usr_message "Create Image" "Path to FliSys HTTP Service not found. Exiting..."
     exit 1
   fi
 
   if test -z "$(file_exists "${DOCKER_FILE}")"; then
-    echo "Dockerfile is missing in HTTP Service path. Exiting..."
+    usr_message "Create Image" "Dockerfile is missing in HTTP Service path. Exiting..."
     exit 1
   fi
 
   image_main_version="$(find_dockerfile_version "${DOCKER_FILE}")"
   if test -z "${image_main_version}"; then
-    echo "Invalid Dockerfile version of image. Exiting..."
+    usr_message "Create Image" "Invalid Dockerfile version of image. Exiting..."
     exit 1
   fi
 
   split_dockerfile_version "${image_main_version}"
 
   if test -z "${BIN_DOCKER}"; then
-    echo "Docker service is not installed or not in your environment variable. Exiting..."
+    usr_message "Create Image" "Docker service is not installed or not in your environment variable. Exiting..."
     exit 1
   fi
 
   if test -z "$(check_docker_service)"; then
-    echo "Docker service is not running. Please, start it before proceed."
-    echo "Try this command as root: systemctl start docker"
+    usr_message "Create Image" "Docker service is not running. Please, start it before proceed."
+    echo -e "\tTry this command as root: systemctl start docker"
     echo "Exiting..."
     exit 1
   fi
@@ -99,16 +107,21 @@ function main() {
 
     # Check if user aswered it
     if test -z "${user_choice}"; then
-      echo "You must choose a valid option, otherwise can not proceed. Exiting..."
+      usr_message "Create Image" "You must choose a valid option, otherwise can not proceed. Exiting..."
       exit 1
     elif test "${user_choice}" = "n"; then
-      echo -n "You choosed not delete an image of FliSys HTTP Service that is at same version. "
-      echo "In this case, it is impossible to proceed once it will be overwritten. Exiting..."
+      usr_message "Create Image" "You choosed not delete an image of FliSys HTTP Service that is at same version. In this case, it is impossible to proceed once it will be overwritten. Exiting..."
       exit 0
     fi
 
     delete_image "${IMAGE_HTTP}"
   fi
+
+  echo "${image_main_version}"
+  echo "${DOCKERF_VER_MAJOR}"
+  echo "${DOCKERF_VER_MID}"
+  echo "${DOCKERF_VER_MINOR}"
+  exit 0
   
   # create a new image
   docker_build_image "${IMAGE_HTTP}" "${image_main_version}"
@@ -123,6 +136,46 @@ function main() {
   echo "all good"
 }
 
+function set_environment() {
+  local file_path
+
+  file_path="$(dirname "$(dirname "${SCRIPT_PATH}")")"
+
+  if test ! -z "$(echo "${CONTAINER_ENVIRONMENT}" | grep -E 'production')"; then
+    DOCKER_FILE="${file_path}/Dockerfile"
+    usr_message "Create Image" "Set environment as Production"
+  else
+    DOCKER_FILE="${file_path}/Dockerfile-dev"
+    usr_message "Create Image" "Set environment as Production"
+  fi
+}
+
+# ########################################################################## #
+# Helper functions
+# ########################################################################## #
+
+function get_arguments() {
+  # check if have something to process
+  if test "${#}" -eq 0; then
+    return
+  fi
+
+  # get arguments
+  while test "${#}" -gt 0; do
+    case "${1}" in
+      --environment=*) CONTAINER_ENVIRONMENT="$(echo "${1#*=}" | tr '[:upper:]' '[:lower:]')"; shift 1;; # string
+      *) usr_message "Create Image" "Unknown option: ${1}"; exit 1;;
+    esac
+  done
+
+  # check for error
+  if test -z "${CONTAINER_ENVIRONMENT}" -o -z "$(echo "${CONTAINER_ENVIRONMENT}" | grep -E '(production|development)')"; then
+    usr_message "Create Image" 'Invalid argument: <environment>. Should be "production" or "development".'
+    exit 1
+  fi
+}
+
+# ########################################################################## #
 # Start the script
-# ###########################################################################
+# ########################################################################## #
 main "${@}"
--- docker_config/scripts/util.sh
+++ docker_config/scripts/util.sh
@@ -61,6 +61,15 @@ function check_bash_version() {
   fi
 }
 
+function usr_message() {
+  # check if have something to process
+  if test "${#}" -ne 2; then
+    return
+  fi
+
+  printf "%s: %s\n" "${1}" "${2}"
+}
+
 # Check if a given path exists
 function path_exists() {
   # check if have something to process
@@ -97,7 +106,7 @@ function find_dockerfile_version() {
     exit 1
   fi
 
-  dck_img_ver="$(grep -i "flisys_version" "${1}" | sed 's/"//g' | awk -F "=" '{ print $2 }')"
+  dck_img_ver="$(grep -i "flisys_version" "${1}" | sed 's/"//g' | awk -F "=" '{ print $2 }' | awk '{ print $1 }')"
   
   if test -z "$(echo "${dck_img_ver}" | grep -E '^"?[0-9]+\.[0-9]+\.[0-9]+"?$')"; then
     echo "Invalid Dockerfile version of image. Exiting..."
@@ -119,13 +128,13 @@ function split_dockerfile_version() {
   DOCKERF_VER_MID="$(echo "${1}" | sed 's/"//g' | awk -F "." '{ print $2 }')"
   DOCKERF_VER_MINOR="$(echo "${1}" | sed 's/"//g' | awk -F "." '{ print $3 }')"
 
-  if test -z "$(echo ${DOCKERF_VER_MAJOR} | grep -E '^[0-9]+$')"; then
+  if test -z "$(echo "${DOCKERF_VER_MAJOR}" | grep -E '^[0-9]+$')"; then
     echo "Invalid major version from Dockerfile. Exiting..."
     exit 1
-  elif test -z "$(echo ${DOCKERF_VER_MID} | grep -E '^[0-9]+$')"; then
+  elif test -z "$(echo "${DOCKERF_VER_MID}" | grep -E '^[0-9]+$')"; then
     echo "Invalid middle version from Dockerfile. Exiting..."
     exit 1
-  elif test -z "$(echo ${DOCKERF_VER_MINOR} | grep -E '^[0-9]+$')"; then
+  elif test -z "$(echo "${DOCKERF_VER_MINOR}" | grep -E '^[0-9]+$')"; then
     echo "Invalid minor version from Dockerfile. Exiting..."
     exit 1
   fi
@@ -410,6 +419,7 @@ function docker_build_image() {
 # Export Global Functions
 # ###########################################################################
 export -f check_bash_version
+export -f usr_message
 export -f path_exists
 export -f file_exists
 export -f find_dockerfile_version
